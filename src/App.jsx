3233eb0d38be14e982eddaa335d42c5baabb5bc3
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-05-29 20:50:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Card } from './components/Card'
+
+export function App() {
+  return (
+    <>
+      <h1 className='mb-4'>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </>
+  )
+}
